e437ff456fc5c65bafa32db4240fce6b91f1f191
Jeremy N. Surma <surma@dag.jsurma.local>
2020-10-17 23:42:00 -0500

    Binary to decimal can’t be that hard, right?

--- interrupts/irq.s
+++ interrupts/irq.s
@@ -3,6 +3,11 @@ PORTA = $6001
 DDRB = $6002
 DDRA = $6003
 
+value = $0200    ; 2 bytes
+mod10 = $0202    ; 2 bytes
+message = $0204  ; 6 bytes
+
+
 E  = %10000000
 RW = %01000000
 RS = %00100000
@@ -27,13 +32,91 @@ reset:
   lda #$00000001 ; Clear display
   jsr lcd_instruction
 
-  lda #"4"
+  lda #0
+  sta message
+  
+  ; Initialize value to be the number to convert 
+  lda number
+  sta value
+  lda number + 1
+  sta value + 1
+
+divide:  
+  ; Initialize the remainder to zero
+  lda #0
+  sta mod10
+  sta mod10 + 1
+  clc
+  
+  ldx #16
+divloop:
+  ; Rotating quotient and remainder
+  rol value
+  rol value + 1
+  rol mod10
+  rol mod10 + 1
+  
+  ; a,y = dividend - divisor
+  sec
+  lda mod10
+  sbc #10
+  tay ;save low byte in Y
+  lda mod10 + 1
+  sbc #0
+  bcc ignore_result ; branch if dividend < divisor
+  sty mod10
+  sta mod10 + 1
+  
+ignore_result:
+  dex
+  bne divloop
+  rol value ; shift in the last bit of the quotient
+  rol value + 1
+  
+  lda mod10
+  clc
+  adc #"0"
+  jsr push_char
+
+  ; if value !- 0, then continue dividing
+  lda value
+  ora value +1
+  bne divide ; branch if value != 0
+  
+  ldx #0
+print:
+  lda message,x
+  beq loop
   jsr print_char
-  lda #"2"
-  jsr print_char
+  inx
+  jmp print
   
 loop:
   jmp loop
+
+number: .word 1729
+
+; Add the character in the A register to the beginning of the
+; null-terminated string `message`
+
+push_char:
+  pha ; Push new first char onto stack
+  ldy #0
+  
+char_loop:  
+  lda message,y ; Get char on string and put into X
+  tax
+  pla
+  sta message,y ; Pull char off stack and add it to the string
+  iny
+  txa
+  pha           ; Push char from string onto stack
+  bne char_loop
+  
+  pla
+  sta message,y ; Pull the null off the stack and add to the end of the string
+  
+  rts
 
 lcd_wait:
   pha
@@ -76,6 +159,7 @@ print_char:
   lda #RS         ; Clear E bits
   sta PORTA
   rts
+
 
 nmi:
 irq:
